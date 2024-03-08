78489e87e124547d1df029813846d81e39b0e827
YosheeJain <yosheejain@gmail.com>
2024-03-07 18:40:03 -0600

    producer sol completed

---
+++ Tech-Lab-On-Campus/Producer-And-Consumer/producer/solution/producer_sol.py
@@ -0,0 +1,28 @@
+from producer_interface import mqProducerInterface
+import pika
+import os
+
+class mqProducer(mqProducerInterface):
+    def __init__(self, exchange_name, routing_key):
+        self.exchange_name = exchange_name
+        self.routing_key = routing_key
+        self.channel = None
+        self.connection = None
+        self.setupRMQConnection()
+
+    def setupRMQConnection(self):
+        con_params = pika.URLParameters(os.environ["AMQP_URL"])
+        self.connection = pika.BlockingConnection(parameters=con_params)
+        # print("connection setup")
+        self.channel = self.connection.channel()
+        self.channel.exchange_declare(exchange=self.exchange_name)
+        # print("channel setup")
+
+    def publishOrder(self, message):
+        self.channel.basic_publish(
+            exchange=self.exchange_name,
+            routing_key=self.routing_key,
+            body=message,
+        )
+        self.channel.close()
+        self.connection.close()
